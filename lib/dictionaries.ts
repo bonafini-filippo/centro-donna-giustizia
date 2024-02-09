68cece919eafd8316e9d91d9dc7c424374b21df1
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-02-09 21:54:34 +0100

    dict

--- lib/dictionaries.ts
+++ lib/dictionaries.ts
@@ -6,6 +6,6 @@ const dictionaries = {
     nl: () => import('@/dictionares/nl.json').then((module) => module.default),
 }
 
-/* export const getDictionary = async (locale: keyof typeof dictionaries) => dictionaries[locale]();
- */
-export const getDictionary = async (locale: keyof typeof dictionaries) => dictionaries["it"]();
+export const getDictionary = async (locale: keyof typeof dictionaries) => dictionaries[locale]();
+
+// export const getDictionary = async (locale: keyof typeof dictionaries) => dictionaries["it"]();
