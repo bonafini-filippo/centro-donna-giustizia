06cc76de55455c7cc034ced2b6c2fadfb7c95f85
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-03-23 18:11:26 +0100

    a

--- components/develope-message.tsx
+++ components/develope-message.tsx
@@ -3,7 +3,7 @@ import { FaGear } from 'react-icons/fa6'
 
 const DevelopeMessage = () => {
     return (
-        <div className="my-32 flex justify-center items-center text-4xl pt-20 text-primary font-bold uppercase gap-4">
+        <div className="my-32 flex justify-center items-center text-xl pt-20 text-primary font-bold uppercase gap-4">
             <FaGear size={60} className="animate-spin" />  In fase di sviluppo<FaGear size={60} className="animate-spin" />
         </div>
     )
